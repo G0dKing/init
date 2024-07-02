a4b85dec0a7dedc1652b5a7bd223736cf2329ff7
Alex Pariah <68952952+G0dKing@users.noreply.github.com>
2024-07-02 16:27:00 -0400

    Delete init/tools directory

--- init/tools/bg_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# >>> Execute one or more commands as background processes and suppress output
-# >>> Usage: `bgRun [CMD_1] [CMD_2] [...]`
-
-bg_run() {
-    eval "$@" &>/dev/null & disown;
-}
